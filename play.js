ed0dee54559da4cfa58e70b44370287d8086053e
Zach Harrison <zharrison100@gmail.com>
2020-12-02 15:41:47 +0000

    initial setup complete

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  
+  // USE EVENT HANDLER TO HANDLE INCOMING DATA AND LOG IT TO THE CONSOLE.
+  conn.on('data', (data) => {
+    console.log(data)
+  });
+
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
