a6ddc5bcdbc071291056551e80a4002988806046
Zach Harrison <zharrison100@gmail.com>
2020-12-02 15:50:17 +0000

    added client.js

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-  // USE EVENT HANDLER TO HANDLE INCOMING DATA AND LOG IT TO THE CONSOLE.
-  conn.on('data', (data) => {
-    console.log(data)
-  });
-
-
-  return conn;
-}
-
+// IMPORT CONNECT FUNCTION FROM CLIENT.JS
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
